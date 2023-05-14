a83e642afcfb1f1dc550f3858ae0420fb0b6ae20
Bruno Nunes Cunha <brunonunes2505@gmail.com>
2023-05-14 19:27:22 -0300

    criação do projeto listagem de pokemon

---
+++ src/js/index.js
@@ -0,0 +1,15 @@
+const botaoTrocaDeTema = document.getElementById("botao-alterar-tema")
+const body = document.querySelector("body")
+const imagemBotaoTrocaDeTema = document.querySelector(".imagem-botao")
+
+botaoTrocaDeTema.addEventListener("click", () => {
+    const modoEscuroEstaAtivo = body.classList.contains("modo-escuro")
+
+    body.classList.toggle("modo-escuro")
+
+    if (modoEscuroEstaAtivo) {
+        imagemBotaoTrocaDeTema.setAttribute("src", "src/img/sun.png")
+    } else {
+        imagemBotaoTrocaDeTema.setAttribute("src", "src/img/moon.png")
+    }
+})
